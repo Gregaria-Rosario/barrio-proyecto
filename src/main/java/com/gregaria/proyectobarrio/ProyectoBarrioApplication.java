9e26ddfbf5f516ff3a9ae3e0ef66cafaa7624a43
Pablo Colaiacovo <pablo.colaiacovo@gmail.com>
2021-01-20 18:48:48 -0300

    First commit

---
+++ src/main/java/com/gregaria/proyectobarrio/ProyectoBarrioApplication.java
@@ -0,0 +1,13 @@
+package com.gregaria.proyectobarrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBarrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBarrioApplication.class, args);
+	}
+
+}
