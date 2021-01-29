dc62739d007d0a08de5cb0ee758c7148d5d0bfe2
Gisele Galaburri <gisele.galaburri89@gmail.com>
2021-01-29 11:50:06 -0300

    Created Initiative Service and Repository

---
+++ src/main/java/com/gregaria/proyectobarrio/repositories/InitiativeRepository.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.gregaria.proyectobarrio.repositories;
+
+/**
+ * @author usuario1
+ *
+ */
+public class InitiativeRepository {
+
+}
---
+++ src/main/java/com/gregaria/proyectobarrio/services/InitiativeService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.gregaria.proyectobarrio.services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InitiativeService {
+	
+	@Transactional
+	public void create() {
+		
+	}
+
+}
