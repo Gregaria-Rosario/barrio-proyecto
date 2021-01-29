1694bfc6448216d7925b11ef0524d6ed37056e56
Mariela Devito <marieladevito@gmail.com>
2021-01-29 11:48:56 -0300

    created Initiative Entity

---
+++ src/main/java/com/gregaria/proyectobarrio/entities/Initiative.java
@@ -0,0 +1,47 @@
+package com.gregaria.proyectobarrio.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.gregaria.proyectobarrio.Enums.State;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Initiative {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+	private String nombre;
+	//private List<User> volunteers;
+	//private User creator;
+	private Integer budget; //presupuesto
+	private String description;
+	private State state;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updatedAt;
+	
+	private Boolean active;
+	private List<Tag> tags;
+	private List<Comment> comments;
+	private String idModification;
+	private List<Vote> editedVotes;
+	private List<String> pictures;
+	private List<Inspiration> inspirations;
+	
+	
+	
+}
