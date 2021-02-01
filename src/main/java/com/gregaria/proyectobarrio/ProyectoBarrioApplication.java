7b057771a2bb4aae9dbb6ac260679a26faed2bcd
Gisele Galaburri <gisele.galaburri89@gmail.com>
2021-02-01 13:50:25 -0300

    Integrated Swagger for APIs documentation

--- src/main/java/com/gregaria/proyectobarrio/ProyectoBarrioApplication.java
+++ src/main/java/com/gregaria/proyectobarrio/ProyectoBarrioApplication.java
@@ -2,12 +2,26 @@ package com.gregaria.proyectobarrio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.gregaria.proyectobarrio.config.SwaggerConfig;
 
 @SpringBootApplication
-public class ProyectoBarrioApplication {
+@Import(SwaggerConfig.class)
+public class ProyectoBarrioApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoBarrioApplication.class, args);
 	}
 
+	  @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	 
+	           registry.addResourceHandler("swagger-ui.html")
+	                    .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    }
+	
 }
