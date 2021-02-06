57e3e3c8f1954fcf838159a8e305922977282849
Pablo Colaiacovo <pablo.colaiacovo@gmail.com>
2021-02-06 11:00:47 -0300

    Updated configs for swagger and included different boot package

--- src/main/java/com/gregaria/proyectobarrio/ProyectoBarrioApplication.java
+++ src/main/java/com/gregaria/proyectobarrio/ProyectoBarrioApplication.java
@@ -1,12 +1,11 @@
 package com.gregaria.proyectobarrio;
 
+import com.gregaria.proyectobarrio.config.SwaggerConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.gregaria.proyectobarrio.config.SwaggerConfig;
 
 @SpringBootApplication
 @Import(SwaggerConfig.class)
@@ -15,10 +14,4 @@ public class ProyectoBarrioApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoBarrioApplication.class, args);
 	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-	}
-
 }
--- src/main/java/com/gregaria/proyectobarrio/config/SwaggerConfig.java
+++ src/main/java/com/gregaria/proyectobarrio/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ package com.gregaria.proyectobarrio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
