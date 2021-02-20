0037e04bb22354254f50e8d7d4f25be05e184ca1
Gisele Galaburri <gisele.galaburri89@gmail.com>
2021-02-20 19:23:17 -0300

    Updated controller and service to save initiatives

--- src/main/java/com/gregaria/proyectobarrio/restcontrollers/InitiativeRestController.java
+++ src/main/java/com/gregaria/proyectobarrio/restcontrollers/InitiativeRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gregaria.proyectobarrio.Enums.State;
 import com.gregaria.proyectobarrio.entities.Initiative;
 import com.gregaria.proyectobarrio.services.InitiativeService;
 
@@ -29,10 +30,40 @@ public class InitiativeRestController {
 		return initiativeService.listActives();
 	}
 	
-	@PostMapping("/save")
-	public @ResponseBody Initiative newInitiative(@RequestParam String title){
-		Initiative response = initiativeService.save(initiative);
-		return new ResponseEntity<>(response, HttpStatus.OK);
+	@PostMapping("/publish")
+	public @ResponseBody Initiative newInitiative(
+			@RequestParam String title,
+			@RequestParam String  creatorId,
+			@RequestParam Integer budget,
+			@RequestParam String description,
+			@RequestParam String idTags
+			){
+		
+		// confirmar como llegan las etiquetas
+		// podria ser un string de ID's de etiquetas separados por coma
+		// como envia desde el front la localizacion? No lo agregué
+		State state = State.PUBLISHED;
+		
+		Initiative initiative = initiativeService.save(title, creatorId, budget, description, idTags, null, state);
+		return initiative;
+	}
+	
+	@PostMapping("/save-draft")
+	public @ResponseBody Initiative saveDraft(
+			@RequestParam String title,
+			@RequestParam String  creatorId,
+			@RequestParam Integer budget,
+			@RequestParam String description,
+			@RequestParam String idTags
+			){
+		
+		// confirmar como llegan las etiquetas
+		// podria ser un string de ID's de etiquetas separados por coma
+		// como envia desde el front la localizacion? No lo agregué
+		State state = State.DRAFT;
+		
+		Initiative initiative = initiativeService.save(title, creatorId, budget, description, idTags, null, state);
+		return initiative;
 	}
 	
 }
--- src/main/java/com/gregaria/proyectobarrio/services/InitiativeService.java
+++ src/main/java/com/gregaria/proyectobarrio/services/InitiativeService.java
@@ -3,6 +3,8 @@
  */
 package com.gregaria.proyectobarrio.services;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,8 +12,12 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gregaria.proyectobarrio.Enums.State;
 import com.gregaria.proyectobarrio.entities.Initiative;
+import com.gregaria.proyectobarrio.entities.Location;
+import com.gregaria.proyectobarrio.entities.Tag;
 import com.gregaria.proyectobarrio.repositories.InitiativeRepository;
 
 
@@ -21,13 +27,63 @@ public class InitiativeService {
 	@Autowired
 	InitiativeRepository initiativeRepository;
 	
+//	@Autowired
+//	UserRepository userRepositiry;
+	
+	@Autowired
+	TagService tagService;
+	
 	@Transactional
-	public Initiative save(Initiative initiative) {
+	public Initiative save(String title,
+			 String  creatorId,
+			 Integer budget,
+			 String description,
+			 String idTags, 
+			 Location location,
+			 State state) {
+		
+		Initiative initiative = new Initiative();
+		
+//		User creator = userRepository.findyById(creatorId);
+//		initiative.setCreator(creator);
+		
+		initiative.setTitle(title);
+		initiative.setBudget(budget);
+		initiative.setDescription(description);
+		
+		List<Tag> tags = tagService.idsToList(idTags);
+				
+		initiative.setTags(tags);
+		initiative.setLocation(location);
+		initiative.setState(state);
+		initiative.setCreatedAt(new Date());
+		initiative.setActive(true);
+		
 		return initiativeRepository.save(initiative);
 	}
 	
 	@Transactional
-	public Initiative update(Initiative initiative) {
+	public Initiative update(String id, String title,
+			 Integer budget,
+			 String description,
+			 String idTags, 
+			 Location location,
+			 State state) throws Exception {
+		
+		Initiative initiative = findById(id);
+			
+		initiative.setTitle(title);
+		initiative.setBudget(budget);
+		initiative.setDescription(description);
+		
+		List<Tag> tags = tagService.idsToList(idTags);
+				
+		initiative.setTags(tags);
+		initiative.setLocation(location);
+		initiative.setState(state);
+		initiative.setUpdatedAt(new Date());
+		
+		
 		return initiativeRepository.save(initiative);
 	}
 	
@@ -36,13 +92,15 @@ public class InitiativeService {
 		return initiativeRepository.findByActiveTrue();
 	}
 	
-	public Initiative findById(String id) {
+	public Initiative findById(String id) throws Exception {
 		Optional<Initiative> response = initiativeRepository.findById(id);
 		
 		Initiative initiative = null;
 		if (response.isPresent()) {
 			 initiative = response.get();
-		} 
+		} else {
+			throw new Exception("No se encontró la iniciativa");
+		}
 		return initiative;
 	}
 	
